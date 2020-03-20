c9ad0d72c9c5c693a5f21ef360cf387348fe35a6
Mykola.Somakin <k.semakin@gmail.com>
2020-03-20 21:29:20 +0200

    Updates

---
+++ precache-manifest.673e75f2c2c4cd4512e095c98bce0ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921b469cd843c0e7076ea3adefeb00a4",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ebbb52b63a64519bbab3",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.56cac4f6.chunk.css"
+  },
+  {
+    "revision": "25c83bb2722336f0133a",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.68a20725.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.68a20725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbb52b63a64519bbab3",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fcdb2ab2.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
